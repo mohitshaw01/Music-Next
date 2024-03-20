4bb24d8a68bbe1a648d9223d96ccb41c242b4e88
MOHIT KUMAR SHAW <mohitshawmath@gmail.com>
2024-03-20 16:31:27 +0530

    scrolling done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import { Stick } from "next/font/google";
+import StickyScroll from "@/components/StickyScroll";
 
 export default function Home() {
   return (
     <div>
       <h1>Next.js + TypeScript</h1>
-      <HeroSection /> 
+      <HeroSection />
+      <FeaturedCourses /> 
+      <StickyScroll />
     </div>
   );
 }
