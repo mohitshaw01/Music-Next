63d36623b36e9f1cef3cabad0f57e65ba9d02214
MOHIT KUMAR SHAW <mohitshawmath@gmail.com>
2024-03-22 18:10:18 +0530

    Add image domains to next.config.js and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
-import { Stick } from "next/font/google";
-import StickyScroll from "@/components/StickyScroll";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from '@/components/TestimonialCards';
+import UpcomingWebinars from "@/components/upcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <h1>Next.js + TypeScript</h1>
       <HeroSection />
       <FeaturedCourses /> 
-      <StickyScroll />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </div>
   );
 }
